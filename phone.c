f7fe8cf5418f9cf9c1b8f5fb7477f23209babfb2
Duy14102 <bongdien77@gmail.com>
2021-11-23 16:33:07 +0700

    BPL

--- phone.c
+++ phone.c
@@ -25,18 +25,26 @@ typedef struct
     char note[100];
     int favourite;
 } contact;
+// Menu of first view
 void menu();
-void searchcontact(contact contacts[], int *count);
-contact addcontact(contact contacts[], int *count);
-int contactdetails(contact contacts[], int *choices, int *count2);
 void favouritescontact(contact *contacts, int *count);
 void allcontact(contact *contacts, int *count);
-void geta();
-contact addfavourite(contact *contacts, int *count2);
+void searchcontact(contact contacts[], int *count);
+contact addcontact(contact contacts[], int *count);
+// Menu of contact detail
+int contactdetails(contact contacts[], int *choices, int *count2);
 void contactdetailsmenu(contact contacts[], int choice, int *count2);
+contact addfavourite(contact *contacts, int *count2);
 contact contactupdate(contact contacts[], int *count);
 void delete (contact *contacts, int *choices, int *count2);
 void addfield(contact *contacts, int choices, int *count2);
+//Check input function
+char checkinputphone(char *phone);
+char checkinputbirthday(char *birthday);
+int CheckName(char *name);
+bool isValidEmailAddress(char *EmailAddress);
+// Get string from struct function
+void geta();
 //File
 int WriteFile(contact *contacts, int count);
 int ReadFile(contact *contacts, int *count);
@@ -80,7 +88,7 @@ int main(int argCount, char **args)
     } while (choice != 5);
     return 0;
 }
-void menu()
+void menu() //Get Menu For Main()
 {
     system("cls");
     printf("========================================\n");
@@ -94,7 +102,7 @@ void menu()
     printf("========================================\n");
     printf("Your Choice : ");
 }
-contact addcontact(contact contacts[], int *count)
+contact addcontact(contact contacts[], int *count) //Function 1 : Add contact
 {
     system("cls");
     ReadFile(contacts, count);
@@ -104,20 +112,50 @@ contact addcontact(contact contacts[], int *count)
     printf("=======================================\n");
     printf("first name : ");
     geta(contacts->firstname, 100);
+    while (CheckName(contacts->firstname) == 1)
+    {
+        printf("Invalid name, re-enter : ");
+        geta(contacts->firstname, 100);
+    }
     printf("middle name : ");
     geta(contacts->middlename, 100);
+    while (CheckName(contacts->middlename) == 1)
+    {
+        printf("Invalid name, re-enter : ");
+        geta(contacts->middlename, 100);
+    }
     printf("last name : ");
     geta(contacts->lastname, 100);
+    while (CheckName(contacts->lastname) == 1)
+    {
+        printf("Invalid name, re-enter : ");
+        geta(contacts->lastname, 100);
+    }
     printf("company : ");
     geta(contacts->company, 100);
     printf("phone : ");
     geta(contacts->phone, 100);
+    while (CheckName(contacts->phone) == 0)
+    {
+        printf("Invalid phone, re-enter : ");
+        geta(contacts->phone, 100);
+    }
     printf("email : ");
     geta(contacts->email, 100);
+    while (!isValidEmailAddress(contacts->email))
+    {
+        printf("Invalid Email, re-enter : ");
+        geta(contacts->email, 100);
+    }
     printf("address : ");
     geta(contacts->address, 100);
     printf("birthday : ");
     geta(contacts->birthday, 100);
+    while (CheckName(contacts->birthday) == 0)
+    {
+        printf("Invalid phone, re-enter : ");
+        geta(contacts->birthday, 100);
+    }
     printf("website : ");
     geta(contacts->website, 100);
     printf("note : ");
@@ -129,7 +167,7 @@ contact addcontact(contact contacts[], int *count)
     getchar();
     return *contacts;
 }
-void searchcontact(contact contacts[], int *count)
+void searchcontact(contact contacts[], int *count) //Function 2 : Search contact
 {
     ReadFile(contacts, count);
     system("cls");
@@ -137,7 +175,7 @@ void searchcontact(contact contacts[], int *count)
     int count_search = 0;
     char name[100];
     int i, choice, save[100];
-    if (*count == 0)
+    if (*count == 0) // check count from file
     {
         printf("Contacts not exists\n");
         printf("Press any key to continue\n");
@@ -145,7 +183,6 @@ void searchcontact(contact contacts[], int *count)
     }
     else
     {
-        //co ten
         printf("Input to search : ");
         gets(name);
         printf("+----------------------------------------------------------------+\n");
@@ -168,7 +205,6 @@ void searchcontact(contact contacts[], int *count)
                         printf(" ");
                     }
                     printf("|\n");
-                    // }
                 }
                 count_search++;
             }
@@ -201,7 +237,7 @@ void searchcontact(contact contacts[], int *count)
     }
 }
 
-void geta(char *str, int length)
+void geta(char *str, int length) // Get string
 {
     fseek(stdin, 0, SEEK_END);
     fflush(stdin);
@@ -210,7 +246,7 @@ void geta(char *str, int length)
     fseek(stdin, 0, SEEK_END);
     fflush(stdin);
 }
-void favouritescontact(contact contacts[], int *count)
+void favouritescontact(contact contacts[], int *count) //Function 3 : Favourite
 {
     ReadFile(contacts, count);
     system("cls");
@@ -226,7 +262,7 @@ void favouritescontact(contact contacts[], int *count)
     printf("+----------------------------------------------------------------+\n");
     for (i = 0; i < *count; i++)
     {
-        if (contacts[i].favourite == 1)
+        if (contacts[i].favourite == 1) //fv = 1(exists) , fv = 0(!exists)
         {
             int NO = i + 1;
             int lengthname = strlen(contacts[i].firstname) + strlen(contacts[i].lastname);
@@ -268,7 +304,7 @@ void favouritescontact(contact contacts[], int *count)
         contactdetailsmenu(contacts, save[choice], count);
     }
 }
-void allcontact(contact contacts[], int *count)
+void allcontact(contact contacts[], int *count) //Function 4 : All contact
 {
     ReadFile(contacts, count);
     system("cls");
@@ -321,7 +357,7 @@ void allcontact(contact contacts[], int *count)
         contactdetailsmenu(contacts, choice - 1, count);
     }
 }
-void contactdetailsmenu(contact contacts[], int choice, int *count2)
+void contactdetailsmenu(contact contacts[], int choice, int *count2) //Get Detail from contact
 {
     ReadFile(contacts, count2);
     int i = choice;
@@ -345,11 +381,11 @@ void contactdetailsmenu(contact contacts[], int choice, int *count2)
     }
     contactdetails(contacts, &choice, count2);
 }
-int contactdetails(contact contacts[], int *choices, int *count2)
+int contactdetails(contact contacts[], int *choices, int *count2) //Main of detail work
 {
     ReadFile(contacts, count2);
     int choice;
-    if (contacts[*choices].favourite == 0)
+    if (contacts[*choices].favourite == 0) // Menu if fv = 0(!exists)
     {
         printf("=======================================\n");
         printf("1.Add to Favourites\n");
@@ -358,7 +394,7 @@ int contactdetails(contact contacts[], int *choices, int *count2)
         printf("4.Addfield\n");
         printf("5.Back to menu\n");
     }
-    else if (contacts[*choices].favourite == 1)
+    else if (contacts[*choices].favourite == 1) // Menu if fv = 1(exists)
     {
         printf("=======================================\n");
         printf("1.Delete Favourites\n");
@@ -396,7 +432,7 @@ int contactdetails(contact contacts[], int *choices, int *count2)
         break;
     }
 }
-contact addfavourite(contact *contacts, int *count2)
+contact addfavourite(contact *contacts, int *count2) // Function 1 : Favourite
 {
     ReadFile(contacts, count2);
     if (contacts[*count2].favourite == 0)
@@ -415,7 +451,7 @@ contact addfavourite(contact *contacts, int *count2)
     }
     return *contacts;
 }
-contact contactupdate(contact contacts[], int *count)
+contact contactupdate(contact contacts[], int *count) // Function 2 : Update Field
 {
     ReadFile(contacts, count);
     int choice;
@@ -423,63 +459,108 @@ contact contactupdate(contact contacts[], int *count)
     fflush(stdin);
     printf("Which type do you want to update(only default) ?\n1. FirstName\t2. MiddleName\t3. LastName\t4. Company\n5. Phone\t6. Email\t7. Address\t8. Birthday\n9. Website\t10. Note\t11. Exit\n Your Choice : ");
     scanf("%d", &choice);
+    while (choice < 1 || choice > 11)
+    {
+        printf("Try again : ");
+        scanf("%d", &choice);
+    }
     switch (choice)
     {
     case 1:
+        system("cls");
         printf("New first name : ");
         geta(contacts->firstname, 100);
+        while (CheckName(contacts->firstname) == 1)
+        {
+            printf("Invalid name, re-enter : ");
+            geta(contacts->firstname, 100);
+        }
         printf("Update Complete!\nPress any key to continue...");
         getchar();
         break;
     case 2:
+        system("cls");
         printf("New middle name : ");
         geta(contacts->middlename, 100);
+        while (CheckName(contacts->middlename) == 1)
+        {
+            printf("Invalid name, re-enter : ");
+            geta(contacts->middlename, 100);
+        }
         printf("Update Complete!\nPress any key to continue...");
         getchar();
         break;
     case 3:
+        system("cls");
         printf("New last name : ");
         geta(contacts->lastname, 100);
+        while (CheckName(contacts->lastname) == 1)
+        {
+            printf("Invalid name, re-enter : ");
+            geta(contacts->lastname, 100);
+        }
         printf("Update Complete!\nPress any key to continue...");
         getchar();
         break;
     case 4:
+        system("cls");
         printf("New company : ");
         geta(contacts->company, 100);
         printf("Update Complete!\nPress any key to continue...");
         getchar();
         break;
     case 5:
+        system("cls");
         printf("New phone : ");
         geta(contacts->phone, 100);
+        while (CheckName(contacts->phone) == 0)
+        {
+            printf("Invalid name, re-enter : ");
+            geta(contacts->phone, 100);
+        }
         printf("Update Complete!\nPress any key to continue...");
         getchar();
         break;
     case 6:
+        system("cls");
         printf("New email : ");
         geta(contacts->email, 100);
+        while (!isValidEmailAddress(contacts->email))
+        {
+            printf("Invalid Email, re-enter : ");
+            geta(contacts->email, 100);
+        }
         printf("Update Complete!\nPress any key to continue...");
         getchar();
         break;
     case 7:
+        system("cls");
         printf("New address : ");
         geta(contacts->address, 100);
         printf("Update Complete!\nPress any key to continue...");
         getchar();
         break;
     case 8:
+        system("cls");
         printf("New birthday : ");
         geta(contacts->birthday, 100);
+        while (CheckName(contacts->birthday) == 0)
+        {
+            printf("Invalid name, re-enter : ");
+            geta(contacts->birthday, 100);
+        }
         printf("Update Complete!\nPress any key to continue...");
         getchar();
         break;
     case 9:
+        system("cls");
         printf("New website : ");
         geta(contacts->website, 100);
         printf("Update Complete!\nPress any key to continue...");
         getchar();
         break;
     case 10:
+        system("cls"); 
         printf("New note : ");
         geta(contacts->note, 100);
         printf("Update Complete!\nPress any key to continue...");
@@ -491,7 +572,7 @@ contact contactupdate(contact contacts[], int *count)
     WriteFile(contacts, *count);
     return *contacts;
 }
-void delete (contact *contacts, int *choices, int *count2)
+void delete (contact *contacts, int *choices, int *count2) // Function 3 : Delete Field
 {
     ReadFile(contacts, count2);
     int i, j;
@@ -505,7 +586,7 @@ void delete (contact *contacts, int *choices, int *count2)
     printf("Press any key to continue\n");
     getchar();
 }
-void addfield(contact *contacts, int choices, int *count2)
+void addfield(contact *contacts, int choices, int *count2) // Function 4 : Add Field
 {
     ReadFile(contacts, count2);
     int checkIsfield = 0;
@@ -537,34 +618,130 @@ void addfield(contact *contacts, int choices, int *count2)
     fflush(stdin);
     getchar();
 }
-int WriteFile(contact *contacts, int count)
+int WriteFile(contact *contacts, int count) // Write File
 {
     FILE *file;
     int filesave = 0;
+    // File = .txt, "wb" is create or if !exists->create
     file = fopen("contacts.txt", "wb");
     if (file != NULL)
     {
+        //Write the number of contact to file
         fwrite(&count, sizeof(int), 1, file);
+        //Save the contact to file
         fwrite(contacts, sizeof(contact), count, file);
         fclose(file);
         filesave = 1;
     }
     return filesave;
 }
-int ReadFile(contact *contacts, int *count)
+int ReadFile(contact *contacts, int *count) // Read File
 {
     FILE *file;
     int fileread = 0;
     file = fopen("contacts.txt", "rb");
     if (file != NULL)
     {
+        //Read the number of contact from file
         fread(count, sizeof(int), 1, file);
         if (count > 0)
         {
+            //Read contact from file then save back to contact
             fread(contacts, sizeof(contact), *count, file);
             fileread = 1;
         }
         fclose(file);
     }
     return fileread;
+}
+char checkinputphone(char *phone)
+{
+    int check = 0, check2;
+    do
+    {
+        check = 0;
+        check2 = strlen(phone);
+        if (phone[0] < 48 && phone[0] != 43 || phone[0] > 57)
+        {
+            printf("Invalid phone number!\nPhone: ");
+            geta(phone, 100);
+            check = 1;
+        }
+        else
+        {
+            for (int i = 1; i < check2; i++)
+            {
+                if (phone[i] < 48 || phone[i] > 57)
+                {
+                    printf("Invalid phone number!\nPhone: ");
+                    geta(phone, 100);
+                    check = 1;
+                    break;
+                }
+            }
+        }
+    } while (check == 1);
+    return *phone;
+}
+char checkinputbirthday(char *birthday)
+{
+    int check = 0, check2;
+    do
+    {
+        check = 0;
+        check2 = strlen(birthday);
+        if (birthday[0] < 48 && birthday[0] != 43 || birthday[0] > 57)
+        {
+            printf("Invalid Birthday!\nPhone: ");
+            geta(birthday, 100);
+            check = 1;
+        }
+        else
+        {
+            for (int i = 1; i < check2; i++)
+            {
+                if (birthday[i] < 48 || birthday[i] > 57)
+                {
+                    printf("Invalid Birthday!\nPhone: ");
+                    geta(birthday, 100);
+                    check = 1;
+                    break;
+                }
+            }
+        }
+    } while (check == 1);
+    return *birthday;
+}
+int CheckName(char *name)
+{
+    int correct = 0;
+    for (int i = 0; i < strlen(name); i++)
+    {
+        for (int j = 48; j <= 57; j++)
+        {
+            if (name[i] == j)
+                correct = 1;
+        }
+    }
+    return correct;
+}
+bool isValidEmailAddress(char *EmailAddress)
+{
+    if (!EmailAddress) // If cannot read the Email Address...
+        return 0;
+    int AtOffset = -1;
+    int DotOffset = -1;
+    int Length = strlen(EmailAddress); // Length = StringLength (strlen) of EmailAddress
+    for (int i = 0; i < Length; i++)
+    {
+        if (EmailAddress[i] == '@') // If one of the characters is @, store it's position in AtOffset
+            AtOffset = (int)i;
+        else if (EmailAddress[i] == '.') // Same, but with the dot
+            DotOffset = (int)i;
+    }
+    if (AtOffset == -1 || DotOffset == -1) // If cannot find a Dot or a @
+        return 0;
+    if (AtOffset > DotOffset) // If the @ is after the Dot
+        return 0;
+    return !(DotOffset >= ((int)Length - 1)); //Chech there is some other letters after the Dot
 }
